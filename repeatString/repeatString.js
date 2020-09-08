344610797a3b2ab6a629a8fe1dedd06f07d7a704
mohammed sherif <sherifmohammed657@gmail.com>
2020-09-09 02:29:52 +0800

    fixed repeatString.js and modified repeatString.spec.js

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,4 +1,12 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    let repeat = "";
+    if(num < 0) {
+        return "ERROR";
+    }
+   for(let i = 0; i < num; i++) {
+       repeat += str;
+   }
+   return repeat;  
 
 }
 
