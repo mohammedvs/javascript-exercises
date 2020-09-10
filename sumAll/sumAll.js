fb020cb8fa547b2fd9ebc752d41ce630a7ff31ae
mohammed sherif <sherifmohammed657@gmail.com>
2020-09-10 20:36:17 +0800

    fixed sumAll.js and modified sumAll.spec.js file

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
+    if (x < 0 || y < 0)
+    return "ERROR";
+if (typeof x !== "number" || typeof y !== "number") 
+    return "ERROR";
 
+var finalSum = 0;
+for(let i = 0; i < x + y; i++) {
+    finalSum += i;
 }
+return finalSum; 
+
+};
 
 module.exports = sumAll
