6f91140869e60becab7ec4ec74d2b291f84227d5
mohammed sherif <sherifmohammed657@gmail.com>
2020-09-10 20:46:24 +0800

    added leapYears.js and tempConversion.js

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,8 +1,14 @@
-const ftoc = function() {
+const ftoc = function(fahr) {
+  let cels = Math.round((fahr - 32) * (5/9) * 10) / 10;
+
+  return cels;
 
 }
 
-const ctof = function() {
+const ctof = function(cels) {
+  let fahr = Math.round(((cels * 9/5) + 32) * 10) / 10;
+
+  return fahr;
 
 }
 
